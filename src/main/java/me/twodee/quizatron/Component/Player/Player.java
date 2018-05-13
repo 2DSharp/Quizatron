e36907225eeec7007356d98f09c414fe67b29f04
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2018-05-13 19:38:27 +0530

    Update media player

--- src/main/java/me/twodee/quizatron/Component/Player/Player.java
+++ src/main/java/me/twodee/quizatron/Component/Player/Player.java
@@ -1,11 +1,19 @@
 package me.twodee.quizatron.Component.Player;
 
+import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.skins.JFXSliderSkin;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -14,10 +22,12 @@ import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import javax.inject.Inject;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 
 public class Player {
 
@@ -32,13 +42,18 @@ public class Player {
     @FXML private Button playBtn;
     @FXML private Button pauseBtn;
     @FXML private MediaView mediaBox;
+    @FXML private Label currTimeLbl;
+    @FXML private Label endTimeLbl;
+    @FXML private JFXSlider timeSlider;
 
     @Inject
     public Player(FileChooser fileChooser) {
         this.mediaView = mediaBox;
         this.fileChooser = fileChooser;
     }
-
+    @FXML
+    public void initialize() {
+    }
     public MediaView getMediaView() {
         return mediaView;
     }
@@ -55,6 +70,38 @@ public class Player {
         try {
             String source = file.toURI().toURL().toExternalForm();
             this.loadMedia(source);
+            timeSlider.setValue(this.mediaPlayer.getCurrentTime().toSeconds());
+
+            mediaPlayer.setOnReady(new Runnable() {
+
+                @Override
+                public void run() {
+                    int duration = (int) mediaPlayer.getTotalDuration().toSeconds();
+                    endTimeLbl.setText(String.format("%02d", duration / 60) + ":" +
+                            String.format("%02d", duration  % 60));
+                }
+            });
+
+            this.mediaPlayer.currentTimeProperty().addListener((observableValue, oldDuration, newDuration) -> {
+
+                timeSlider.setValue((newDuration.toSeconds() / this.mediaPlayer.getTotalDuration().toSeconds())
+                        * 100.0);
+                String currentTime = String.format("%02d",(int) (newDuration.toSeconds() / 60))+ ":" +
+                        String.format("%02d", (int) (newDuration.toSeconds() % 60));
+
+                int timeLeft = (int) (this.mediaPlayer.getTotalDuration().toSeconds() - newDuration.toSeconds());
+
+                endTimeLbl.setText(String.format("%02d", timeLeft / 60) + ":" +
+                        String.format("%02d", timeLeft  % 60));
+
+                currTimeLbl.setText(currentTime);
+
+                if (timeLeft == 0) {
+                    this.stop();
+                    timeSlider.setValue(1);
+                }
+
+            });
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -89,14 +136,15 @@ public class Player {
     private void setPlayIcon() {
         FontAwesomeIconView playIcon = new FontAwesomeIconView(FontAwesomeIcon.PLAY);
         playIcon.setGlyphSize(16);
-        playIcon.setStyleClass("trackBtn");
+        playIcon.setStyleClass("trackBtnIcon");
         playBtn.setGraphic(playIcon);
         playBtn.setOnAction(this::play);
     }
 
     private void setPauseIcon() {
         FontAwesomeIconView pauseIcon = new FontAwesomeIconView(FontAwesomeIcon.PAUSE);
-        pauseIcon.setStyleClass("trackBtn");
+
+        pauseIcon.setStyleClass("trackBtnIcon");
         pauseIcon.setGlyphSize(16);
         playBtn.setGraphic(pauseIcon);
         playBtn.setOnAction(this::pause);
@@ -114,4 +162,24 @@ public class Player {
         mediaPlayer.stop();
         setPlayIcon();
     }
+    /*
+    public void bindToTime(Label timer, Text dater) {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0),
+                        new EventHandler<ActionEvent>() {
+                            @Override public void handle(ActionEvent actionEvent) {
+                                Calendar time = Calendar.getInstance();
+                                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+                                timer.setText(simpleDateFormat.format(time.getTime()));
+                                SimpleDateFormat date = new SimpleDateFormat("EEEE, dd MMMM");
+                                dater.setText(date.format(time.getTime()));
+                            }
+                        }
+                ),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+    */
 }
