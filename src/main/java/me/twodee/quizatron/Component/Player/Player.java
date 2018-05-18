bc2083d3930ff2d55fd9fc99f13d193cb7298749
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2018-05-19 03:43:00 +0530

    Clean up code and document

--- src/main/java/me/twodee/quizatron/Component/Player/Player.java
+++ src/main/java/me/twodee/quizatron/Component/Player/Player.java
@@ -7,11 +7,12 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableMap;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -28,12 +29,15 @@ import java.io.File;
 import static java.lang.Math.abs;
 
 /**
- * @author dedipyaman
- * @version %I%, %G%
+ * Media Player component for Quizatron
+ * @author Dedipyaman Das <2d@twodee.me>
+ * @version 2018
  * @since 1.0.18.1
  */
 public class Player {
 
+    private static final String PLAY = "play";
+    private static final String PAUSE = "pause";
     private MediaView mediaView;
     private MediaPlayer mediaPlayer;
     private FileChooser fileChooser;
@@ -41,31 +45,26 @@ public class Player {
 
     @FXML private AnchorPane playerNode;
     @FXML private Button playBtn;
-    @FXML private Button pauseBtn;
-    @FXML private MediaView mediaBox;
     @FXML private Label currTimeLbl;
     @FXML private Label endTimeLbl;
     @FXML private JFXSlider timeSlider;
     @FXML private Label mediaInfo;
     @FXML private Label sourceFileLbl;
-    private boolean sliderChanging;
-
     /**
      * Media player component constructor.
-     * It has to be injected with a {@link FileChooser} for getting the media file, {@link MediaView}
-     * @see Presentation
-     * @param fileChooser
-     * @param mediaView
-     * @param presentation
+     * Guice or any other injector needs to inject a {@link FileChooser} for getting the media file,
+     * {@link MediaView} for visual output
+     * and {@link Presentation} to have it presented in a separate view
+     * @param fileChooser FileChooser - single file
+     * @param mediaView MediaView - Parent container for visual output
+     * @param presentation Presentation state for separate stage
      */
     @Inject
     public Player(FileChooser fileChooser, MediaView mediaView, Presentation presentation) {
         this.mediaView = mediaView;
         this.fileChooser = fileChooser;
         this.presentation = presentation;
-        sliderChanging = false;
     }
-
     /**
      * Initializer to get the FXML components working.
      */
@@ -75,15 +74,8 @@ public class Player {
         prepareMediaView();
     }
     /**
-     * Sets the presentation view
-     * @param presentation
-     */
-    public void setPresentation(Presentation presentation) {
-        this.presentation = presentation;
-    }
-    /**
      * Loads the media and instantiates a new media player
-     * @param source Media file source location
+     * @param source String- Media file source location
      */
     public void loadMedia(String source) {
 
@@ -107,8 +99,8 @@ public class Player {
     }
     /**
      * Open the file chooser and autoplay the media
-     * @param event Mouse click
-     * @throws Exception
+     * @param event ActionEvent
+     * @throws Exception NullPointerException thrown on failure to open file
      */
     @FXML private void chooseMediaFile(ActionEvent event) throws Exception{
 
@@ -122,7 +114,7 @@ public class Player {
      * Plays the file
      * Loads the media file and initializes the slider
      * Plays the given media file
-     * @param source
+     * @param source String - source file to be played
      */
     private void play(String source) {
 
@@ -133,7 +125,6 @@ public class Player {
         initUIControlsBehavior();
         playMedia();
     }
-
     /**
      * Fetches and substitutes the placeholders for media metadata
      */
@@ -141,70 +132,76 @@ public class Player {
 
         mediaPlayer.setOnReady(() -> {
 
-                String artistName = (String) mediaPlayer.getMedia().getMetadata().get("artist");
-                String title = (String)mediaPlayer.getMedia().getMetadata().get("title");
-                String album = (String)mediaPlayer.getMedia().getMetadata().get("album");
-                String mediaSource = mediaPlayer.getMedia().getSource();
+            ObservableMap<String, Object> metaData = mediaPlayer.getMedia().getMetadata();
 
-                mediaSource = mediaSource.replace("%20", " ");
-                mediaInfo.setText(title+ " - " + artistName + " - " + album);
-                sourceFileLbl.setText(mediaSource.substring( mediaSource.lastIndexOf('/')+1,
-                        mediaSource.length()));
+            String artistName = (String) metaData.get("artist");
+            String title = (String) metaData.get("title");
+            String album = (String) metaData.get("album");
+            String mediaSource = mediaPlayer.getMedia().getSource();
 
-                int duration = (int) mediaPlayer.getTotalDuration().toSeconds();
-                endTimeLbl.setText(String.format("%02d", duration / 60) + ":" +
-                        String.format("%02d", duration  % 60));
-                }
-        );
+            mediaSource = mediaSource.replace("%20", " ");
+            mediaInfo.setText(title+ " - " + artistName + " - " + album);
+            sourceFileLbl.setText(mediaSource.substring( mediaSource.lastIndexOf('/')+1,
+                    mediaSource.length()));
+
+            int duration = (int) mediaPlayer.getTotalDuration().toSeconds();
+            endTimeLbl.setText(String.format("%02d", duration / 60) + ":" +
+                    String.format("%02d", duration  % 60));
+            });
     }
-
+    /**
+     * Initialize the slider behavior
+     */
     private void initializeTimeSlider() {
 
         initSliderSeekBehavior();
         initSliderProgressBehavior();
     }
-
     /**
      * Sets the behavior of the player UI based on the player state
      */
     private void initUIControlsBehavior() {
-
+        // Compromising on the readability a bit, maybe switch to a more verbose approach?
         mediaPlayer.setOnEndOfMedia(() -> {
             stop();
             timeSlider.setValue(1);
         });
 
         mediaPlayer.setOnPaused(() -> {
-            setPlayIcon();
+            setPlayPauseIcon(PLAY);
         });
 
         mediaPlayer.setOnPlaying(() -> {
-            setPauseIcon();
+            setPlayPauseIcon(PAUSE);
         });
-
     }
+    /**
+     * The slider behavior on dragging/clicking on the JFXSlider to seek
+     * @see JFXSlider
+     */
     private void initSliderSeekBehavior() {
         timeSlider.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-
-                if (abs(oldValue.doubleValue() - newValue.doubleValue()) > 1) {
-
+                // Is the change significant enough?
+                // Drag was buggy, have to run some tests
+                if (abs(oldValue.doubleValue() - newValue.doubleValue()) > 0.1) {
                     mediaPlayer.seek(Duration.millis(newValue.doubleValue() *
                             mediaPlayer.getTotalDuration().toMillis() / 100.0));
                 }
             }
         });
     }
-    @FXML private void clickDetect(MouseEvent event) {
-        sliderChanging = true;
-    }
-
+    /**
+     * Behavior of the slider as it progresses
+     * Setting the time elapsed and time left on the appropriate indicators
+     */
     private void initSliderProgressBehavior() {
 
         this.mediaPlayer.currentTimeProperty().addListener((observableValue, oldDuration, newDuration) -> {
-
+            // Making sure it doesn't interfere with the manual seeking
             if (!timeSlider.isValueChanging()) {
+
                 timeSlider.setValue((newDuration.toMillis() / this.mediaPlayer.getTotalDuration().toMillis())
                         * 100.0);
                 String currentTime = String.format("%02d",(int) (newDuration.toSeconds() / 60))+ ":" +
@@ -212,7 +209,7 @@ public class Player {
 
                 double realTimeLeft = (mediaPlayer.getTotalDuration().toSeconds() - newDuration.toSeconds());
                 int timeLeft = (int) realTimeLeft;
-
+                // Time elapsed/left indicators update
                 endTimeLbl.setText(String.format("%02d", timeLeft / 60) + ":" +
                         String.format("%02d", timeLeft  % 60));
                 currTimeLbl.setText(currentTime);
@@ -221,17 +218,24 @@ public class Player {
     }
     /**
      * Setter for the class media player
-     * @param mediaPlayer
+     * @param mediaPlayer MediaPlayer {@link MediaPlayer}
      */
     private void setMediaPlayer(MediaPlayer mediaPlayer) {
         this.mediaPlayer = mediaPlayer;
     }
-
+    /**
+     * User action to play the media
+     * @param event ActionEvent
+     */
     @FXML
     private void play(ActionEvent event) {
         playMedia();
     }
-
+    /**
+     * Prepare the external presentation view
+     * Get the view controller and embed visual output
+     * {@link Presentation}
+     */
     private void preparePresentation() {
 
         mediaView.setMediaPlayer(this.mediaPlayer);
@@ -247,7 +251,9 @@ public class Player {
             e.printStackTrace();
         }
     }
-
+    /**
+     * Check if the media player was already there, prepare the presentation and play the video
+     */
     private void playMedia() {
 
         if (mediaView.getMediaPlayer() != this.mediaPlayer) {
@@ -255,27 +261,39 @@ public class Player {
         }
         mediaPlayer.play();
     }
-
+    /**
+     * User action event to pause the video
+     * @param event
+     */
     @FXML private void pause(ActionEvent event) {
 
         mediaPlayer.pause();
     }
+    /**
+     * Change the pause button to a play button and have the appropriate action based on it
+     * {@link FontAwesomeIconView}
+     */
+    private void setPlayPauseIcon(String status) {
 
-    private void setPlayIcon() {
-        FontAwesomeIconView playIcon = new FontAwesomeIconView(FontAwesomeIcon.PLAY);
-        playIcon.setGlyphSize(16);
-        playIcon.setStyleClass("trackBtnIcon");
-        playBtn.setGraphic(playIcon);
-        playBtn.setOnAction(this::play);
+        FontAwesomeIconView icon;
+        if (status == PLAY) {
+            icon= new FontAwesomeIconView(FontAwesomeIcon.PLAY);
+            playBtn.setOnAction(this::play);
+        }
+        else {
+            icon = new FontAwesomeIconView(FontAwesomeIcon.PAUSE);
+            playBtn.setOnAction(this::pause);
+        }
+        icon.setGlyphSize(16);
+        icon.setStyleClass("trackBtnIcon");
+        playBtn.setGraphic(icon);
     }
-
-    private void setPauseIcon() {
-
-        FontAwesomeIconView pauseIcon = new FontAwesomeIconView(FontAwesomeIcon.PAUSE);
-        pauseIcon.setStyleClass("trackBtnIcon");
-        pauseIcon.setGlyphSize(16);
-        playBtn.setGraphic(pauseIcon);
-        playBtn.setOnAction(this::pause);
+    /**
+     * User action event to stop the media
+     */
+    public void stop() {
+        mediaPlayer.stop();
+        setPlayPauseIcon(PLAY);
     }
 
     public void openPlaylist() {
@@ -287,9 +305,6 @@ public class Player {
     public void next() {
 
     }
-    public void stop() {
-        mediaPlayer.stop();
-        setPlayIcon();
-    }
+
 
 }
