b5e976717df8b59f346b832790cef6ca4696381c
Payne Miller <paynemiller7792@gmail.com>
2018-10-23 14:04:06 -0400

    Fixed Sequence unit tests (#14)
    * Fixed Sequence unit tests
    
    * Merge redundant tests into one test

--- src/test/java/me/twodee/quizatron/Model/Mapper/SequenceMapperTest.java
+++ src/test/java/me/twodee/quizatron/Model/Mapper/SequenceMapperTest.java
@@ -1,46 +1,56 @@
 package me.twodee.quizatron.Model.Mapper;
 
+import me.twodee.quizatron.Component.CSVManager;
+import me.twodee.quizatron.Model.Contract.IMapper;
 import me.twodee.quizatron.Model.Entity.Configuration.Configuration;
+import me.twodee.quizatron.Model.Entity.Sequence;
+import me.twodee.quizatron.Model.Exception.NonExistentRecordException;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
-public class SequenceMapperTest
-{
+public class SequenceMapperTest {
     @Mock
     private Configuration configuration;
-/*
-    @Before
-    public void beforeEachTest() throws URISyntaxException
-    {
-        String sequenceFile = Paths.get(this.getClass().getResource("/sequence.csv").toURI())
-                            .toAbsolutePath()
-                            .toString();
 
+    private IMapper sequenceMapper;
+
+    @Before
+    public void beforeEachTest() throws URISyntaxException, IOException {
+        String sequenceFile = Paths.get(this.getClass().getResource("/rounds/sequences.csv").toURI())
+                .toAbsolutePath()
+                .toString();
+        CSVManager csvManager = new CSVManager();
+        sequenceMapper = new CSVSequenceMapper(csvManager, sequenceFile);
+        sequenceMapper.init();
         MockitoAnnotations.initMocks(this);
         when(configuration.getSequence()).thenReturn(sequenceFile);
     }
 
-    @Test (expected = NonExistentRecordException.class)
-    public void fetchNonExistentIndexPositive() throws NonExistentRecordException, IOException
-    {
-        CSVManager csvManager = new CSVManager();
-        IMapper sequenceMapper = new CSVSequenceMapper(csvManager);
-        ((CSVSequenceMapper) sequenceMapper).init(configuration);
+    @Test(expected = NonExistentRecordException.class)
+    public void fetchNonExistentIndexPositive() throws NonExistentRecordException {
+
         Sequence sequence = new Sequence();
         sequence.setIndex(10);
         sequenceMapper.fetch(sequence);
     }
 
     @Test
-    public void fetchEmptySequenceTest() throws IOException, NonExistentRecordException
-    {
-        CSVManager csvManager = new CSVManager();
-        IMapper sequenceMapper = new CSVSequenceMapper(csvManager);
-        ((CSVSequenceMapper) sequenceMapper).init(configuration);
+    public void fetchEmptySequenceTest() throws NonExistentRecordException {
         Sequence sequence = new Sequence();
         sequenceMapper.fetch(sequence);
         String result = sequence.getName();
@@ -49,11 +59,7 @@ public class SequenceMapperTest
     }
 
     @Test
-    public void fetchFirstRecordTest() throws IOException, NonExistentRecordException
-    {
-        CSVManager csvManager = new CSVManager();
-        IMapper sequenceMapper = new CSVSequenceMapper(csvManager);
-        ((CSVSequenceMapper) sequenceMapper).init(configuration);
+    public void fetchFirstRecordTest() throws NonExistentRecordException {
         Sequence sequence = new Sequence();
 
         sequence.setIndex(0);
@@ -63,11 +69,7 @@ public class SequenceMapperTest
     }
 
     @Test
-    public void fetchSecondRecordTest() throws IOException, NonExistentRecordException
-    {
-        CSVManager csvManager = new CSVManager();
-        IMapper sequenceMapper = new CSVSequenceMapper(csvManager);
-        ((CSVSequenceMapper) sequenceMapper).init(configuration);
+    public void fetchSecondRecordTest() throws NonExistentRecordException {
         Sequence sequence = new Sequence();
 
         sequence.setIndex(1);
@@ -77,78 +79,32 @@ public class SequenceMapperTest
     }
 
     @Test
-    public void totalRecordNumTest() throws IOException
-    {
-        CSVManager csvManager = new CSVManager();
-        IMapper sequenceMapper = new CSVSequenceMapper(csvManager);
-        ((CSVSequenceMapper) sequenceMapper).init(configuration);
-
+    public void totalRecordNumTest() {
         int result = sequenceMapper.getTotalRecords();
-
         assertThat(result, is(6));
     }
 
     @Test
-    public void fetchTypeTest() throws IOException, NonExistentRecordException
-    {
-        CSVManager csvManager = new CSVManager();
-        IMapper sequenceMapper = new CSVSequenceMapper(csvManager);
-        ((CSVSequenceMapper) sequenceMapper).init(configuration);
+    public void fetchFileTest() throws NonExistentRecordException {
         Sequence sequence = new Sequence();
-
-        sequence.setIndex(3);
+        sequence.setIndex(5);
         sequenceMapper.fetch(sequence);
-
+        assertThat(sequence.getFilePath(), is("rapidfire.csv"));
+        assertThat(sequence.getIntro(), is("red.mp4"));
         assertThat(sequence.getType(), is("block"));
     }
 
     @Test
-    public void fetchFileTest() throws IOException, NonExistentRecordException
-    {
-        CSVManager csvManager = new CSVManager();
-        IMapper sequenceMapper = new CSVSequenceMapper(csvManager);
-        ((CSVSequenceMapper) sequenceMapper).init(configuration);
-        Sequence sequence = new Sequence();
-
-        sequence.setIndex(5);
-        sequenceMapper.fetch(sequence);
-
-        assertThat(sequence.getFilePath(), is("rapidfire.csv"));
-    }
-
-    @Test
-    public void fetchIntroTest() throws IOException, NonExistentRecordException
-    {
-        CSVManager csvManager = new CSVManager();
-        IMapper sequenceMapper = new CSVSequenceMapper(csvManager);
-        ((CSVSequenceMapper) sequenceMapper).init(configuration);
-        Sequence sequence = new Sequence();
-
-        sequence.setIndex(3);
-        sequenceMapper.fetch(sequence);
-
-        assertThat(sequence.getIntro(), is("yellow.mp4"));
-    }
-
-    @Test
-    public void streamTest() throws IOException
-    {
-        CSVManager csvManager = new CSVManager();
-        IMapper sequenceMapper = new CSVSequenceMapper(csvManager);
-        ((CSVSequenceMapper) sequenceMapper).init(configuration);
-
+    public void streamTest() {
         Stream<Sequence> sequences = sequenceMapper.getStream();
+        List<String> names = sequences.map(Sequence::getName)
+                .collect(Collectors.toList());
 
-         List<String> names = sequences.map(Sequence::getName)
-                                       .collect(Collectors.toList());
-
-         assertThat(names, hasItems("Regular Round",
-                             "Audio Visual Round",
-                             "Subject Round",
-                             "Who Am I?",
-                             "BlockStage Round",
-                             "Rapid Fire"));
+        assertThat(names, hasItems("Regular Round",
+                "Audio Visual Round",
+                "Subject Round",
+                "Who Am I?",
+                "BlockStage Round",
+                "Rapid Fire"));
     }
-*/
-
 }
